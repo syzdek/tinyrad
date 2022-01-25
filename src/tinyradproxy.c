dd2425ed48c6d471c1208fbd6ce8b3bcb48ee2d8
David M. Syzdek <david@syzdek.net>
2022-01-24 16:54:15 -0900

    tinyradproxy: cleaning up header include order

--- src/tinyradproxy.c
+++ src/tinyradproxy.c
@@ -38,14 +38,14 @@
 ///////////////
 #pragma mark - Headers
 
+#include "common.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <strings.h>
 #include <unistd.h>
 #include <getopt.h>
-
-#include "common.h"
 
 
 ///////////////////
