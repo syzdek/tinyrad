c580a29eb3d7518ebbe9b926b2b657f7fb3d9aaf
David M. Syzdek <david@syzdek.net>
2022-01-27 20:13:40 -0900

    tests: adding test-dict-attrs

---
+++ tests/dict-data.c
@@ -0,0 +1,275 @@
+/*
+ *  Tiny RADIUS Client Library
+ *  Copyright (C) 2022 David M. Syzdek <david@syzdek.net>.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are
+ *  met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of David M. Syzdek nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ *  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ *  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL DAVID M. SYZDEK BE LIABLE FOR
+ *  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ *  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ *  SUCH DAMAGE.
+ */
+#define _TESTS_DICT_DATA_C 1
+#include "dict-data.h"
+
+
+///////////////
+//           //
+//  Headers  //
+//           //
+///////////////
+#pragma mark - Headers
+
+#include <stddef.h>
+#include <assert.h>
+
+#include "common.h"
+
+
+///////////////////
+//               //
+//  Definitions  //
+//               //
+///////////////////
+#pragma mark - Definitions
+
+
+/////////////////
+//             //
+//  Variables  //
+//             //
+/////////////////
+#pragma mark - Variables
+
+#pragma mark test_dict_data_attrs[]
+// RADIUS attributes: https://www.iana.org/assignments/radius-types/radius-types.xhtml
+const TinyRadDictAttrDef test_dict_data_attrs[] =
+{
+   { "Syzdek-HTML-Color",         26,      27893,            0,   TRAD_INTEGER,              0 },  // custom attribute for testing
+   { "Syzdek-Faith",              26,      27893,            1,   TRAD_INTEGER,              0 },  // custom attribute for testing
+   { "Syzdek-Animal-Group",       26,      27893,            2,   TRAD_INTEGER,              0 },  // custom attribute for testing
+   { "TinyrRad-Foobar",          224,          0,            0,    TRAD_STRING,              0 },  // custom attribute for testing
+   { "TinyrRad-Signature",       225,          0,            0,    TRAD_OCTETS,              0 },  // custom attribute for testing
+   { "TinyrRad-YoYo-Make",       226,          0,            0,   TRAD_INTEGER,              0 },  // custom attribute for testing
+   { NULL, 0, 0, 0, 0, 0 }
+};
+
+
+#pragma mark test_dict_data_values[]
+const TinyRadDictValueDef test_dict_data_values[] =
+{
+   { "Syzdek-Animal-Group",       "Invertebrates",             1 },  // custom value for testing
+   { "Syzdek-Animal-Group",       "Fish",                      2 },  // custom value for testing
+   { "Syzdek-Animal-Group",       "Amphibians",                3 },  // custom value for testing
+   { "Syzdek-Animal-Group",       "Reptiles",                  4 },  // custom value for testing
+   { "Syzdek-Animal-Group",       "Birds",                     5 },  // custom value for testing
+   { "Syzdek-Animal-Group",       "Mammals",                   6 },  // custom value for testing
+
+   { "Syzdek-HTML-Color",         "Black",                     1 },  // custom value for testing
+   { "Syzdek-HTML-Color",         "White",                     2 },  // custom value for testing
+   { "Syzdek-HTML-Color",         "Gray",                      3 },  // custom value for testing
+   { "Syzdek-HTML-Color",         "Silver",                    4 },  // custom value for testing
+   { "Syzdek-HTML-Color",         "Maroon",                    5 },  // custom value for testing
+   { "Syzdek-HTML-Color",         "Red",                       6 },  // custom value for testing
+   { "Syzdek-HTML-Color",         "Purple",                    7 },  // custom value for testing
+   { "Syzdek-HTML-Color",         "Fushsia",                   8 },  // custom value for testing
+   { "Syzdek-HTML-Color",         "Green",                     9 },  // custom value for testing
+   { "Syzdek-HTML-Color",         "Lime",                     10 },  // custom value for testing
+   { "Syzdek-HTML-Color",         "Olive",                    11 },  // custom value for testing
+   { "Syzdek-HTML-Color",         "Yellow",                   12 },  // custom value for testing
+   { "Syzdek-HTML-Color",         "Navy",                     13 },  // custom value for testing
+   { "Syzdek-HTML-Color",         "Blue",                     14 },  // custom value for testing
+   { "Syzdek-HTML-Color",         "Teal",                     15 },  // custom value for testing
+   { "Syzdek-HTML-Color",         "Aqua",                     16 },  // custom value for testing
+
+   { "Syzdek-Faith",              "Unknown",                   1 },  // custom value for testing
+   { "Syzdek-Faith",              "Other",                     2 },  // custom value for testing
+   { "Syzdek-Faith",              "Catholic",                  3 },  // custom value for testing
+   { "Syzdek-Faith",              "Roman-Rite",                3 },  // custom value for testing
+   { "Syzdek-Faith",              "Ritus-Romanus",             3 },  // custom value for testing
+   { "Syzdek-Faith",              "Roman-Catholic",            3 },  // custom value for testing
+   { "Syzdek-Faith",              "Christian",                 4 },  // custom value for testing
+   { "Syzdek-Faith",              "Judaism",                   5 },  // custom value for testing
+
+   { "TinyrRad-YoYo-Make",        "Unknown",                   0 },  // custom value for testing
+   { "TinyrRad-YoYo-Make",        "YoYoFactory",               1 },  // custom value for testing
+   { "TinyrRad-YoYo-Make",        "Duncan",                    2 },  // custom value for testing
+   { "TinyrRad-YoYo-Make",        "iYoYo",                     3 },  // custom value for testing
+   { "TinyrRad-YoYo-Make",        "YoYoTricks-com",            4 },  // custom value for testing
+   { "TinyrRad-YoYo-Make",        "Magic-Yoyo",                5 },  // custom value for testing
+   { "TinyrRad-YoYo-Make",        "Recess",                    6 },  // custom value for testing
+   { "TinyrRad-YoYo-Make",        "Throwback-Skill-Toys",      7 },  // custom value for testing
+   { "TinyrRad-YoYo-Make",        "yoyo-friends",              8 },  // custom value for testing
+
+   { NULL, NULL, 0}
+};
+
+
+#pragma mark test_dict_data_vendors[]
+// See https://www.iana.org/assignments/enterprise-numbers/enterprise-numbers)
+const TinyRadDictVendorDef test_dict_data_vendors[] =
+{
+   { "ACS",              8838,   1, 1 },  // ACS Internet, Inc. (https://www.iana.org/assignments/enterprise-numbers/enterprise-numbers)
+   { "Syzdek",          27893,   1, 1 },  // DMS  (https://www.iana.org/assignments/enterprise-numbers/enterprise-numbers)
+   { "RFC-Example",     32473,   1, 1 },  // Enterprise Number for Documentation Use (RFC 5612)
+   { NULL, 0, 0, 0 }
+};
+
+
+/////////////////
+//             //
+//  Functions  //
+//             //
+/////////////////
+#pragma mark - Functions
+
+void
+our_dict_diag_attr(
+         unsigned                      opts,
+         const TinyRadDictAttrDef *    def,
+         TinyRadDictAttr *             attr )
+{
+   char *      attr_name;
+   uint8_t     attr_type;
+   uint8_t     attr_type_octets;
+   uint8_t     attr_len_octets;
+   uint32_t    attr_vendor_id;
+   uint32_t    attr_vendor_type;
+   uint32_t    attr_data_type;
+   uint32_t    attr_flags;
+   unsigned    attr_ref_count;
+
+   if ((def))
+   {
+      our_verbose(opts, "Attribute Definition");
+      our_verbose(opts, "   Name:             %s",       def->name);
+      our_verbose(opts, "   Type:             %" PRIu8,  def->type);
+      our_verbose(opts, "   Vendor ID:        %" PRIu32, def->vendor_id);
+      our_verbose(opts, "   Vendor Type:      %" PRIu32, def->vendor_type);
+      our_verbose(opts, "   Data Type:        %" PRIu32, def->data_type);
+      our_verbose(opts, "   Flags:            0x%08x",   (unsigned)def->flags);
+   };
+
+   if ((attr))
+   {
+      attr_name         = NULL;
+      attr_type         = 0;
+      attr_vendor_id    = 0;
+      attr_vendor_type  = 0;
+      attr_data_type    = 0;
+      attr_flags        = 0;
+      attr_type_octets  = 0;
+      attr_len_octets   = 0;
+      attr_ref_count    = 0;
+      tinyrad_dict_attr_info(attr, TRAD_DICT_OPT_NAME,         &attr_name);
+      tinyrad_dict_attr_info(attr, TRAD_DICT_OPT_TYPE,         &attr_type);
+      tinyrad_dict_attr_info(attr, TRAD_DICT_OPT_VEND_ID,      &attr_vendor_id);
+      tinyrad_dict_attr_info(attr, TRAD_DICT_OPT_VEND_TYPE,    &attr_vendor_type);
+      tinyrad_dict_attr_info(attr, TRAD_DICT_OPT_DATA_TYPE,    &attr_data_type);
+      tinyrad_dict_attr_info(attr, TRAD_DICT_OPT_FLAGS,        &attr_flags);
+      tinyrad_dict_attr_info(attr, TRAD_DICT_OPT_TYPE_OCTS,    &attr_type_octets);
+      tinyrad_dict_attr_info(attr, TRAD_DICT_OPT_LEN_OCTS,     &attr_len_octets);
+      tinyrad_dict_attr_info(attr, TRAD_DICT_OPT_REF_COUNT,    &attr_ref_count);
+      our_verbose(opts, "Attribute Definition");
+      our_verbose(opts, "   Name:             %s",       (((attr_name)) ? attr_name : ""));
+      our_verbose(opts, "   Type:             %" PRIu8,  attr_type);
+      our_verbose(opts, "   Vendor ID:        %" PRIu32, attr_vendor_id);
+      our_verbose(opts, "   Vendor Type:      %" PRIu32, attr_vendor_type);
+      our_verbose(opts, "   Data Type:        %" PRIu32, attr_data_type);
+      our_verbose(opts, "   Flags:            0x%08x",   (unsigned)attr_flags);
+      our_verbose(opts, "   Type Octets:      %" PRIu32, attr_type_octets);
+      our_verbose(opts, "   Length Octets:    %" PRIu32, attr_len_octets);
+      our_verbose(opts, "   Reference Count:  %u",       attr_ref_count);
+   };
+
+   return;
+}
+
+
+int
+our_dict_initialize(
+         unsigned                      opts,
+         TinyRadDict **                dictp )
+{
+   int            rc;
+   char **        errs;
+
+   assert(dictp != NULL);
+
+   *dictp   = NULL;
+   errs     = NULL;
+
+   // initialize dictionary
+   if (tinyrad_dict_initialize(dictp, 0) != TRAD_SUCCESS)
+   {
+      our_error(opts, NULL, "out of virtual memory");
+      return(1);
+   };
+
+   // load builtin/default dictionary objects
+   if ((opts & TRAD_TEST_DFLT_DICT))
+   {
+      our_verbose(opts, "loading default dictionary ...");
+      if ((rc = tinyrad_dict_defaults(*dictp, &errs, 0)) != TRAD_SUCCESS)
+      {
+         our_error(opts, errs, "tinyrad_dict_defaults(): %s", tinyrad_strerror(rc));
+         tinyrad_strings_free(errs);
+         tinyrad_dict_destroy(*dictp);
+         return(1);
+      };
+   };
+
+   // load test dictionary vendors
+   our_verbose(opts, "adding test dictionary vendors ...");
+   if ((rc = tinyrad_dict_import(*dictp, test_dict_data_vendors, NULL, NULL, &errs, 0)) != TRAD_SUCCESS)
+   {
+      our_error(opts, errs, "tinyrad_dict_import(vendors): %s", tinyrad_strerror(rc));
+      tinyrad_strings_free(errs);
+      tinyrad_dict_destroy(*dictp);
+      return(1);
+   };
+
+   // load test dictionary attributes
+   our_verbose(opts, "adding test dictionary attributes ...");
+   if ((rc = tinyrad_dict_import(*dictp, NULL, test_dict_data_attrs, NULL, &errs, 0)) != TRAD_SUCCESS)
+   {
+      our_error(opts, errs, "tinyrad_dict_import(attributes): %s", tinyrad_strerror(rc));
+      tinyrad_strings_free(errs);
+      tinyrad_dict_destroy(*dictp);
+      return(1);
+   };
+
+   // load test dictionary values
+   our_verbose(opts, "adding test dictionary values ...");
+   if ((rc = tinyrad_dict_import(*dictp, NULL, NULL, test_dict_data_values, &errs, 0)) != TRAD_SUCCESS)
+   {
+      our_error(opts, errs, "tinyrad_dict_import(values): %s", tinyrad_strerror(rc));
+      tinyrad_strings_free(errs);
+      tinyrad_dict_destroy(*dictp);
+      return(1);
+   };
+
+   return(0);
+}
+
+
+/* end of source */
---
+++ tests/test-dict-attrs.c
@@ -0,0 +1,329 @@
+/*
+ *  Tiny RADIUS Client Library
+ *  Copyright (C) 2022 David M. Syzdek <david@syzdek.net>.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are
+ *  met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of David M. Syzdek nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ *  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ *  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL DAVID M. SYZDEK BE LIABLE FOR
+ *  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ *  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ *  SUCH DAMAGE.
+ */
+#define _TESTS_TINYRAD_DICT_ATTRS_C 1
+
+
+///////////////
+//           //
+//  Headers  //
+//           //
+///////////////
+#pragma mark - Headers
+
+#include "common.h"
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <assert.h>
+
+#include <tinyrad.h>
+
+#include "dict-data.h"
+
+
+///////////////////
+//               //
+//  Definitions  //
+//               //
+///////////////////
+#pragma mark - Definitions
+
+#undef PROGRAM_NAME
+#define PROGRAM_NAME "tinyrad-dict-attrs"
+
+
+//////////////////
+//              //
+//  Prototypes  //
+//              //
+//////////////////
+#pragma mark - Prototypes
+
+int
+main(
+         int                           argc,
+         char *                        argv[] );
+
+int
+my_verify_attr(
+         unsigned                      opts,
+         const TinyRadDictAttrDef *    def,
+         TinyRadDictAttr *             attr );
+
+
+/////////////////
+//             //
+//  Functions  //
+//             //
+/////////////////
+#pragma mark - Functions
+
+int
+main(
+         int                           argc,
+         char *                        argv[] )
+{
+   int                           opt;
+   int                           c;
+   int                           opt_index;
+   size_t                        pos;
+   unsigned                      opts;
+   TinyRadDict *                 dict;
+   TinyRadDictAttr *             attr;
+   const TinyRadDictAttrDef *    def;
+
+   // getopt options
+   static char          short_opt[] = "dhVvq";
+   static struct option long_opt[] =
+   {
+      {"debug",            no_argument,       NULL, 'd' },
+      {"help",             no_argument,       NULL, 'h' },
+      {"quiet",            no_argument,       NULL, 'q' },
+      {"silent",           no_argument,       NULL, 'q' },
+      {"version",          no_argument,       NULL, 'V' },
+      {"verbose",          no_argument,       NULL, 'v' },
+      { NULL, 0, NULL, 0 }
+   };
+
+   our_initialize(PROGRAM_NAME);
+
+   opts  = TRAD_TEST_DFLT_DICT;
+
+   while((c = getopt_long(argc, argv, short_opt, long_opt, &opt_index)) != -1)
+   {
+      switch(c)
+      {
+         case -1:       /* no more arguments */
+         case 0:        /* long options toggles */
+         break;
+
+         case 'd':
+         opt = TRAD_DEBUG_ANY;
+         tinyrad_set_option(NULL, TRAD_OPT_DEBUG_LEVEL,  &opt);
+         break;
+
+         case 'h':
+         printf("Usage: %s [OPTIONS]\n", PROGRAM_NAME);
+         printf("OPTIONS:\n");
+         printf("  -d, --debug               print debug messages\n");
+         printf("  -h, --help                print this help and exit\n");
+         printf("  -q, --quiet, --silent     do not print messages\n");
+         printf("  -V, --version             print version number and exit\n");
+         printf("  -v, --verbose             print verbose messages\n");
+         printf("\n");
+         return(0);
+
+         case 'q':
+         opts |=  TRAD_TEST_QUIET;
+         opts &= ~TRAD_TEST_VERBOSE;
+         break;
+
+         case 'V':
+         printf("%s (%s) %s\n", PROGRAM_NAME, PACKAGE_NAME, PACKAGE_VERSION);
+         return(0);
+
+         case 'v':
+         opts |=  TRAD_TEST_VERBOSE;
+         opts &= ~TRAD_TEST_QUIET;
+         break;
+
+         case '?':
+         fprintf(stderr, "Try `%s --help' for more information.\n", PROGRAM_NAME);
+         return(1);
+
+         default:
+         fprintf(stderr, "%s: unrecognized option `--%c'\n", PROGRAM_NAME, c);
+         fprintf(stderr, "Try `%s --help' for more information.\n", PROGRAM_NAME);
+         return(1);
+      };
+   };
+
+   // initialize test dictionary
+   if ((our_dict_initialize(opts, &dict)))
+      return(1);
+
+   our_verbose(opts, "Attribute format: type/vendor id/vendor type/name");
+
+   // verifies default attributes in dictionary by name
+   for(pos = 0; ((tinyrad_dict_default_attrs[pos].name)); pos++)
+   {
+      def = &tinyrad_dict_default_attrs[pos];
+      our_verbose(
+         opts,
+         "test attribute search by name %4" PRIu8 "/%" PRIu32 "/%" PRIu32 "  %s ...",
+         def->type,
+         def->vendor_id,
+         def->vendor_type,
+         def->name
+      );
+      attr = tinyrad_dict_attr_lookup(dict, def->name, 0, 0, 0);
+      if ((my_verify_attr(opts, def, attr)))
+      {
+         tinyrad_dict_destroy(dict);
+         return(1);
+      };
+   };
+
+   // verifies test attributes in dictionary by type
+   for(pos = 0; ((test_dict_data_attrs[pos].name)); pos++)
+   {
+      def = &test_dict_data_attrs[pos];
+      our_verbose(
+         opts,
+         "test attribute search by type %4" PRIu8 "/%" PRIu32 "/%" PRIu32 "  %s ...",
+         def->type,
+         def->vendor_id,
+         def->vendor_type,
+         def->name
+      );
+      attr = tinyrad_dict_attr_lookup(dict, NULL, (uint8_t)def->type, def->vendor_id, def->vendor_type);
+      if ((my_verify_attr(opts, def, attr)))
+      {
+         tinyrad_dict_destroy(dict);
+         return(1);
+      };
+   };
+
+   // verifies test attributes in dictionary by name
+   for(pos = 0; ((test_dict_data_attrs[pos].name)); pos++)
+   {
+      def = &test_dict_data_attrs[pos];
+      our_verbose(
+         opts,
+         "default attribute search by name %4" PRIu8 "/%" PRIu32 "/%" PRIu32 "  %s ...",
+         def->type,
+         def->vendor_id,
+         def->vendor_type,
+         def->name
+      );
+      attr = tinyrad_dict_attr_lookup(dict, def->name, 0, 0, 0);
+      if ((my_verify_attr(opts, def, attr)))
+      {
+         tinyrad_dict_destroy(dict);
+         return(1);
+      };
+   };
+
+   // verifies default attributes in dictionary by type
+   for(pos = 0; ((tinyrad_dict_default_attrs[pos].name)); pos++)
+   {
+      def = &tinyrad_dict_default_attrs[pos];
+      our_verbose(
+         opts,
+         "default attribute search by type %4" PRIu8 "/%" PRIu32 "/%" PRIu32 "  %s ...",
+         def->type,
+         def->vendor_id,
+         def->vendor_type,
+         def->name
+      );
+      attr = tinyrad_dict_attr_lookup(dict, NULL, (uint8_t)def->type, def->vendor_id, def->vendor_type);
+      if ((my_verify_attr(opts, def, attr)))
+      {
+         tinyrad_dict_destroy(dict);
+         return(1);
+      };
+   };
+
+   // cleans up dictionary
+   tinyrad_dict_destroy(dict);
+
+   return(0);
+}
+
+
+int
+my_verify_attr(
+         unsigned                      opts,
+         const TinyRadDictAttrDef *    def,
+         TinyRadDictAttr *             attr )
+{
+   uint8_t     u8;
+   uint32_t    u32;
+
+   assert(def != NULL);
+
+   if (!(attr))
+   {
+      our_dict_diag_attr(opts, def, attr);
+      return(our_error(opts, NULL, "attribute was not found"));
+   };
+
+   // compare data types
+   tinyrad_dict_attr_info(attr, TRAD_DICT_OPT_DATA_TYPE, &u32);
+   if (def->data_type != u32)
+   {
+      our_dict_diag_attr(opts, def, attr);
+      return(our_error(opts, NULL, "attribute data types do not match"));
+   };
+
+   // compare types
+   tinyrad_dict_attr_info(attr, TRAD_DICT_OPT_TYPE, &u8);
+   if (def->type != u8)
+   {
+      our_dict_diag_attr(opts, def, attr);
+      return(our_error(opts, NULL, "attribute types do not match"));
+   };
+
+   // compare vendor ID
+   tinyrad_dict_attr_info(attr, TRAD_DICT_OPT_VEND_ID, &u32);
+   if (def->vendor_id != u32)
+   {
+      our_dict_diag_attr(opts, def, attr);
+      return(our_error(opts, NULL, "attribute vendor ID do not match"));
+   };
+
+   // compare vendor types
+   tinyrad_dict_attr_info(attr, TRAD_DICT_OPT_VEND_TYPE, &u32);
+   if (def->vendor_type != u32)
+   {
+      our_dict_diag_attr(opts, def, attr);
+      return(our_error(opts, NULL, "attribute vendor types do not match"));
+   };
+
+   // compare flags
+   tinyrad_dict_attr_info(attr, TRAD_DICT_OPT_FLAGS, &u32);
+   if ((def->flags|TRAD_DFLT_ATTR) != (u32|TRAD_DFLT_ATTR))
+   {
+      our_dict_diag_attr(opts, def, attr);
+      return(our_error(opts, NULL, "attribute flags do not match"));
+   };
+
+   return(0);
+}
+
+
+/* end of source */
+
