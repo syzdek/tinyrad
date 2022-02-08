f00cd15daf06f9880761cb9f5b5b33581e717b3c
David M. Syzdek <david@syzdek.net>
2022-02-08 14:49:24 -0900

    tests: adding test-assertions

---
+++ tests/test-assertions.c
@@ -0,0 +1,164 @@
+/*
+ *  Tiny RADIUS Client Library
+ *  Copyright (C) 2022 David M. Syzdek <david@syzdek.net>.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are
+ *  met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of David M. Syzdek nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ *  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ *  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL DAVID M. SYZDEK BE LIABLE FOR
+ *  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ *  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ *  SUCH DAMAGE.
+ */
+#define _TESTS_TEST_ASSERTIONS_C 1
+
+
+///////////////
+//           //
+//  Headers  //
+//           //
+///////////////
+#pragma mark - Headers
+
+#undef NDEBUG
+
+#include "common.h"
+
+#include <stdio.h>
+#include <getopt.h>
+#include <assert.h>
+
+#include <tinyrad.h>
+
+
+///////////////////
+//               //
+//  Definitions  //
+//               //
+///////////////////
+#pragma mark - Definitions
+
+#undef PROGRAM_NAME
+#define PROGRAM_NAME "test-assertions"
+
+
+//////////////////
+//              //
+//  Prototypes  //
+//              //
+//////////////////
+#pragma mark - Prototypes
+
+int
+main(
+         int                           argc,
+         char *                        argv[] );
+
+
+/////////////////
+//             //
+//  Functions  //
+//             //
+/////////////////
+#pragma mark - Functions
+
+int
+main(
+         int                           argc,
+         char *                        argv[] )
+{
+   int                           c;
+   int                           opt_index;
+   unsigned                      opts;
+
+   // getopt options
+   static char          short_opt[] = "dhVvq";
+   static struct option long_opt[] =
+   {
+      {"debug",            no_argument,       NULL, 'd' },
+      {"help",             no_argument,       NULL, 'h' },
+      {"quiet",            no_argument,       NULL, 'q' },
+      {"silent",           no_argument,       NULL, 'q' },
+      {"version",          no_argument,       NULL, 'V' },
+      {"verbose",          no_argument,       NULL, 'v' },
+      { NULL, 0, NULL, 0 }
+   };
+
+   opts  = 0;
+
+   while((c = getopt_long(argc, argv, short_opt, long_opt, &opt_index)) != -1)
+   {
+      switch(c)
+      {
+         case -1:       /* no more arguments */
+         case 0:        /* long options toggles */
+         break;
+
+         case 'd':
+         break;
+
+         case 'h':
+         printf("Usage: %s [OPTIONS]\n", PROGRAM_NAME);
+         printf("OPTIONS:\n");
+         printf("  -d, --debug               print debug messages\n");
+         printf("  -h, --help                print this help and exit\n");
+         printf("  -q, --quiet, --silent     do not print messages\n");
+         printf("  -V, --version             print version number and exit\n");
+         printf("  -v, --verbose             print verbose messages\n");
+         printf("\n");
+         return(0);
+
+         case 'q':
+         opts |=  TRAD_TEST_QUIET;
+         opts &= ~TRAD_TEST_VERBOSE;
+         break;
+
+         case 'V':
+         printf("%s (%s) %s\n", PROGRAM_NAME, PACKAGE_NAME, PACKAGE_VERSION);
+         return(0);
+
+         case 'v':
+         opts |=  TRAD_TEST_VERBOSE;
+         opts &= ~TRAD_TEST_QUIET;
+         break;
+
+         case '?':
+         fprintf(stderr, "Try `%s --help' for more information.\n", PROGRAM_NAME);
+         return(1);
+
+         default:
+         fprintf(stderr, "%s: unrecognized option `--%c'\n", PROGRAM_NAME, c);
+         fprintf(stderr, "Try `%s --help' for more information.\n", PROGRAM_NAME);
+         return(1);
+      };
+   };
+
+   tinyrad_assertions();
+
+   if ((opts & TRAD_TEST_VERBOSE))
+      printf("%s: It appears that the library's assertions appear correct.\n", PROGRAM_NAME);
+
+   return(0);
+}
+
+
+/* end of source */
+
