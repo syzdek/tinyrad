2d8e7136ff7f5a60759606b7b0b9a0ce6516f289
David M. Syzdek <david@syzdek.net>
2022-02-07 23:59:28 -0900

    libtinyrad: deprecating member values_name_len in TinyRadDictAttr

--- lib/libtinyrad/ldict.h
+++ lib/libtinyrad/ldict.h
@@ -107,7 +107,7 @@ struct _tinyrad_dict_attr
    uint32_t                deprecated_vendor_id;
    uint32_t                vendor_type;
    uint32_t                flags;
-   size_t                  values_name_len;
+   size_t                  deprecated_values_name_len;
    size_t                  deprecated_values_numeric_len;
    TinyRadDictAttr *       first;
    TinyRadDictValue **     deprecated_values_name;
