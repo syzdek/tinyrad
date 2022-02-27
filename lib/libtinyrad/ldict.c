5b159377876ecbee978f53a6a0a5bf365e59ef0b
David M. Syzdek <david@syzdek.net>
2022-02-27 00:43:03 -0900

    libtinyrad: adding __attr_oid_vendor_type()

--- lib/libtinyrad/ldict.c
+++ lib/libtinyrad/ldict.c
@@ -128,6 +128,11 @@ __attr_oid_vendor_id(
          const TinyRadOID *            oid );
 
 
+static uint32_t
+__attr_oid_vendor_type(
+         const TinyRadOID *            oid );
+
+
 static uint8_t
 __attr_type_octs(
          const TinyRadDictAttr *       attr );
@@ -844,6 +849,37 @@ __attr_oid_vendor_id(
 }
 
 
+static uint32_t
+__attr_oid_vendor_type(
+         const TinyRadOID *            oid )
+{
+   assert(oid != NULL);
+   switch(oid->oid_val[0])
+   {
+      case TRAD_ATTR_VENDOR_SPECIFIC:
+      if (oid->oid_len < 2)
+         return(0);
+      return(oid->oid_val[2]);
+
+      case TRAD_ATTR_EXTENDED_ATTRIBUTE_1:
+      case TRAD_ATTR_EXTENDED_ATTRIBUTE_2:
+      case TRAD_ATTR_EXTENDED_ATTRIBUTE_3:
+      case TRAD_ATTR_EXTENDED_ATTRIBUTE_4:
+      case TRAD_ATTR_EXTENDED_ATTRIBUTE_5:
+      case TRAD_ATTR_EXTENDED_ATTRIBUTE_6:
+      if (oid->oid_len < 4)
+         return(0);
+      if (oid->oid_val[1] != 26)
+         return(0);
+      return(oid->oid_val[3]);
+
+      default:
+      break;
+   };
+   return(0);
+}
+
+
 static uint8_t
 __attr_type_octs(
          const TinyRadDictAttr *       attr )
@@ -1183,8 +1219,8 @@ tinyrad_dict_attr_info(
 
       case TRAD_DICT_OPT_VEND_TYPE:
       TinyRadDebug(TRAD_DEBUG_ARGS, "   == %s( attr, TRAD_DICT_OPT_VEND_TYPE, outvalue )", __func__);
-      TinyRadDebug(TRAD_DEBUG_ARGS, "   <= outvalue: \"" PRIu32 "\"", attr->vendor_type);
-      *((uint32_t *)outvalue) = attr->vendor_type;
+      TinyRadDebug(TRAD_DEBUG_ARGS, "   <= outvalue: \"" PRIu32 "\"", __attr_oid_vendor_type(attr->oid));
+      *((uint32_t *)outvalue) = __attr_oid_vendor_type(attr->oid);
       break;
 
       default:
@@ -1984,7 +2020,7 @@ __value_vendor_type(
 {
    assert(value       != NULL);
    assert(value->attr != NULL);
-   return( ((value->attr->vendor)) ? value->attr->vendor_type : 0);
+   return( ((value->attr->vendor)) ? __attr_oid_vendor_type(value->attr->oid) : 0);
 }
 
 
