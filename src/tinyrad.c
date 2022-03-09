2d0251490bde79ec11da842f736a54d962ec1900
David M. Syzdek <david@syzdek.net>
2022-03-08 15:36:11 -0900

    tinyrad: adding file functions

--- src/tinyrad.c
+++ src/tinyrad.c
@@ -47,6 +47,13 @@
 #include <strings.h>
 #include <unistd.h>
 #include <getopt.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <assert.h>
+#include <fcntl.h>
+#include <errno.h>
 
 
 ///////////////////
@@ -62,14 +69,60 @@
 
 //////////////////
 //              //
+//  Data Types  //
+//              //
+//////////////////
+#pragma mark - Data Types
+
+typedef struct my_file_buffer
+{
+   int         fd;         // file descriptor
+   int         padint;
+   char *      eol;        // end of current line
+   char *      bol;        // beginning of current line
+   size_t      len;        // length of current buffer
+   char        buff[256];  // raw buffer
+   char *      filename;
+} MyFile;
+
+
+typedef struct my_request_av
+{
+   char * attr_name;
+   char * attr_value;
+} MyRequestAV;
+
+
+//////////////////
+//              //
 //  Prototypes  //
 //              //
 //////////////////
 #pragma mark - Prototypes
 
-int main( int argc, char * argv[] );
+int
+main(
+         int                           argc,
+         char *                        argv[] );
 
-void my_usage( void );
+
+void
+my_file_close(
+         MyFile *                      fb );
+
+
+MyFile *
+my_file_open(
+         const char *                  filename );
+
+
+int
+my_file_readline(
+         MyFile *                      fb );
+
+
+void
+my_usage( void );
 
 
 /////////////////
@@ -93,10 +146,11 @@ int main(int argc, char * argv[])
    uint8_t        dictloaded;
 
    // getopt options
-   static char          short_opt[] = "D:d:hI:qVv";
+   static char          short_opt[] = "D:d:f:hI:qVv";
    static struct option long_opt[] =
    {
       {"debug",            optional_argument, NULL, 'd' },
+      {"file",             optional_argument, NULL, 'f' },
       {"help",             no_argument,       NULL, 'h' },
       {"quiet",            no_argument,       NULL, 'q' },
       {"silent",           no_argument,       NULL, 'q' },
@@ -228,12 +282,137 @@ int main(int argc, char * argv[])
 }
 
 
+void
+my_file_close(
+         MyFile *                      fb )
+{
+   if (!(fb))
+      return;
+
+   if (fb->fd != -1)
+      close(fb->fd);
+
+   if ((fb->filename))
+      free(fb->filename);
+
+   free(fb);
+
+   return;
+}
+
+
+MyFile *
+my_file_open(
+         const char *                  filename )
+{
+   MyFile *       fb;
+   struct stat    sb;
+   int            rc;
+
+   assert(filename != NULL);
+
+   // verify file exists
+   if ((rc = stat(filename, &sb)) == -1)
+   {
+      our_error(PROGRAM_NAME,  NULL, "%s: %s", filename, strerror(errno));
+      return(NULL);
+   };
+   if ((sb.st_mode & S_IFMT) != S_IFREG)
+   {
+      our_error(PROGRAM_NAME,  NULL, "%s: is not a file", filename);
+      return(NULL);
+   };
+
+   // allocate initial
+   if ((fb = malloc(sizeof(MyFile))) == NULL)
+   {
+      our_error(PROGRAM_NAME,  NULL, "out of virtual memory");
+      return(NULL);
+   };
+   memset(fb, 0, sizeof(MyFile));
+   fb->fd = -1;
+
+   // copy filename
+   if ((fb->filename = strdup(filename)) == NULL)
+   {
+      our_error(PROGRAM_NAME,  NULL, "out of virtual memory");
+      my_file_close(fb);
+      return(NULL);
+   };
+
+   // open file
+   if ((fb->fd = open(filename, O_RDONLY)) == -1)
+   {
+      our_error(PROGRAM_NAME,  NULL, "%s: %s", filename, strerror(errno));
+      my_file_close(fb);
+      return(NULL);
+   };
+
+   return(fb);
+}
+
+
+int
+my_file_readline(
+         MyFile *                      fb )
+{
+   size_t      offset;
+   size_t      pos;
+   size_t      size;
+   ssize_t     rc;
+
+   assert(fb != 0);
+
+   // adjusts beginning of line
+   fb->bol = ((fb->eol)) ? &fb->eol[1] : fb->buff;
+
+   // look for end line
+   if ((fb->eol = strchr(fb->bol, '\n')) != NULL)
+   {
+      fb->eol[0] = '\0';
+      return(1);
+   };
+
+   // shift buffer
+   offset = (size_t)(fb->bol = fb->buff);
+   for(pos = 0; ((pos+offset) < fb->len); pos++)
+      fb->buff[pos] = fb->buff[pos+offset];
+   fb->buff[pos]  = '\0';
+   fb->len       -= offset;
+
+   // attempt to fill buffer
+   size = sizeof(fb->buff) - fb->len - 1;
+   if ((rc = read(fb->fd, &fb->buff[fb->len], size)) == -1)
+   {
+      our_error(PROGRAM_NAME, NULL, "%s: %s", fb->filename, strerror(errno));
+      return(-1);
+   };
+   fb->len           += (size_t)rc;
+   fb->buff[fb->len]  = '\0';
+   fb->bol            = fb->buff;
+   if (!(fb->len))
+      return(0);
+
+   // look for end line
+   if ((fb->eol = strchr(fb->bol, '\n')) != NULL)
+   {
+      fb->eol[0] = '\0';
+      return(1);
+   };
+
+   fb->eol = &fb->bol[strlen(fb->bol)];
+
+   return(0);
+}
+
+
 void my_usage(void)
 {
    printf("Usage: %s [OPTIONS] url\n", PROGRAM_NAME);
    printf("OPTIONS:\n");
    printf("  -D dictionary             include dictionary\n");
    printf("  -d level, --debug=level   print debug messages\n");
+   printf("  -f file, --file=file      attribute list\n");
    printf("  -h, --help                print this help and exit\n");
    printf("  -I path                   add path to dictionary search paths\n");
    printf("  -q, --quiet, --silent     do not print messages\n");
